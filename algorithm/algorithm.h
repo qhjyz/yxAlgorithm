a529323f4c9cc8a3b0303afcf0bb8c5432a54372
xxxoo <xxxoo>
2018-09-18 11:53:47 +0800

    ..

---
+++ algorithm/algorithm.h
@@ -0,0 +1,19 @@
+//
+//  algoritm.h
+//  algoritm
+//
+//  Created by xuwenquan on 2018/9/18.
+//  Copyright © 2018年 Qiaobangzhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for algoritm.
+FOUNDATION_EXPORT double algoritmVersionNumber;
+
+//! Project version string for algoritm.
+FOUNDATION_EXPORT const unsigned char algoritmVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <algoritm/PublicHeader.h>
+
+
